a32c593735b9daa9568452dfd27015b76608e22f
naira <yeghiazaryan.naira1@gmail.com>
2025-06-16 18:07:34 +0400

    feat(components): Implement AppLayout component

---
+++ src/components/AppLayout.jsx
@@ -0,0 +1,34 @@
+import { Layout, Menu } from "antd";
+import {
+  NAVIGATION_ITEMS,
+  SIDER_WIDTH,
+} from "../constants/navigation";
+import "../App.css";
+
+const { Header, Sider, Content } = Layout;
+
+const AppLayout = ({ children, currentView, setCurrentView }) =>  (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Header className="app-header">
+        <h1 className="app-title">Zuegg Store Explorer</h1>
+      </Header>
+      <Layout>
+        <Sider width={SIDER_WIDTH} className="app-sider" theme="light">
+          <Menu
+            mode="vertical"
+            selectedKeys={[currentView]}
+            onClick={({ key }) => setCurrentView(key)}
+          >
+            {NAVIGATION_ITEMS.map((item) => (
+              <Menu.Item key={item.key}>{item.label}</Menu.Item>
+            ))}
+          </Menu>
+        </Sider>
+        <Content className="app-content">
+          {children}
+        </Content>
+      </Layout>
+    </Layout>
+  );
+
+export default AppLayout;
---
+++ src/constants/navigation.js
@@ -0,0 +1,15 @@
+export const NAVIGATION_ITEMS = [
+  {
+    key: "map",
+    label: "Map",
+    cardTitle: "Store Locations",
+  },
+  {
+    key: "charts",
+    label: "Charts",
+    cardTitle: "Store Statistics",
+  },
+];
+
+export const DEFAULT_VIEW = "map";
+export const SIDER_WIDTH = 200;
