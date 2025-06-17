e14ee3409677c696f4fd312b196df58c9c7ff102
naira <yeghiazaryan.naira1@gmail.com>
2025-06-17 17:03:38 +0400

    fix(StoreMap): set initial zoom to 5 for broader view

--- src/components/StoreMap.jsx
+++ src/components/StoreMap.jsx
@@ -52,7 +52,7 @@ const StoreMap = () => {
         </Card>
       )}
       <div style={{ height: '600px' }}>
-        <MapContainer center={center} zoom={8} style={{ height: '100%', width: '100%' }}>
+        <MapContainer center={center} zoom={5} style={{ height: '100%', width: '100%' }}>
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
